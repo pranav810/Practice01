165d005a2ac74933379291c0133a7066cf25afff
Pranav <pranav.varshney_cs18@gla.ac.in>
2019-08-21 15:47:02 +0530

    solution1

--- question1/Question1.java
+++ question1/Question1.java
@@ -1,5 +1,14 @@
 package question1;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+
 public class Question1 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException {
+        DataInputStream br = new DataInputStream(System.in);
+        System.out.println("enter string");
+        String s = br.readLine();
+        System.out.println("Welcome," + " " + s + "!");
+
+    }
 }
